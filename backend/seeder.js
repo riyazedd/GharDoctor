63bb42b50fc24a843909813214bca54a7c9d5dab
riyazedd <riyaz.shrestha00@gmail.com>
2026-05-18 15:05:51 +0545

    User Model Created and sample data added using seeder

---
+++ backend/seeder.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import connectDb from './config/db.js';
+// import products from "./data/product.js";
+import users from "./data/users.js";
+// import Product from "./models/productModel.js";
+import User from "./models/userModel.js";
+
+dotenv.config();
+
+connectDb();
+
+const importData= async ()=>{
+    try{
+        // await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUser = await User.insertMany(users);
+
+        const adminUser = createdUser[0]._id;
+
+        // const sampleProducts= products.map((product)=>{
+        //     return {...product}
+        // })
+
+        // await Product.insertMany(sampleProducts);
+        console.log("Data Imported!!!");
+        process.exit();
+
+    }catch(error){
+        console.error(`${error}`);
+        process.exit(1);
+    }
+}
+
+const destroyData = async ()=>{
+    try{
+        // await Product.deleteMany();
+        await User.deleteMany();
+
+        console.log("Data Destroyed!!!");
+        process.exit();
+    }catch(error){
+        console.error(`${error}`);
+        process.exit(1);
+    }
+}
+
+if(process.argv[2]==='-d'){
+    destroyData();
+}else{
+    importData();
+}
